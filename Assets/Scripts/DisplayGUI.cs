be7faa982f4d477fbcd533b39a0b02f7bf943c3b
L4w3s <l4w3s@live.co.uk>
2015-05-13 18:52:10 +0100

    Revert "Revert "Increased the stat/store sliding sensitivity, added the money background to the money on the main game screen.""
    This reverts commit b2a26acd1cdb54c410231891116f00941a30c3da.

--- Assets/Scripts/DisplayGUI.cs
+++ Assets/Scripts/DisplayGUI.cs
@@ -2,20 +2,25 @@ using UnityEngine;
 using System.Collections;
 
 public class DisplayGUI : MonoBehaviour {	
-	public Texture deleteSaveButton;
+	public Texture moneyBackground;
 
 	void Start () {
 	}
 
 	void OnGUI () {
 
-		if (!GameControl.control.pause) {
-			GUI.BeginGroup(new Rect (0, 0, Screen.width, Screen.height)); // left, top, width, height
+		//if (!GameControl.control.pause) {
+		GUI.BeginGroup(new Rect (0, 0, Screen.width, Screen.height)); // left, top, width, height
+		if (GameControl.control.pause) {
+			GUI.Label(new Rect(10, (20 * GameControl.control.screenMulti), 100, 100), "Lvl " + Mathf.Floor(GameControl.control.level).ToString(), GameControl.control.cowText);
+		} else {
 			GUI.Label(new Rect(10, (20 * GameControl.control.screenMulti), 100, 100), "Lvl " + Mathf.Floor(GameControl.control.level).ToString(), GameControl.control.text);
-
-			GUI.Label(new Rect(Screen.width - (130 * GameControl.control.screenMulti), (20 * GameControl.control.screenMulti), 100, 100), "$" + GameControl.control.money, GameControl.control.text);			
-			GUI.EndGroup();
-			return;
 		}
+
+		GUI.DrawTexture(new Rect(Screen.width - (100 * GameControl.control.screenMulti), 5 * GameControl.control.screenMulti, 90, 45), moneyBackground);
+		GUI.Label(new Rect(Screen.width - (70 * GameControl.control.screenMulti), (10 * GameControl.control.screenMulti), 100, 100), "" + GameControl.control.money, GameControl.control.cowText);
+		GUI.EndGroup();
+		return;
+		//}
 	}
 }
