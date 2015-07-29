b3a2263404e91cb75a2cde46ee1b2fcb2258bb2c
Sam Gallagher <avonwodahs@gmail.com>
2015-07-28 20:39:30 -0400

    UI Scaling tweak

--- Assets/Scripts/UI/DisplayGUI.cs
+++ Assets/Scripts/UI/DisplayGUI.cs
@@ -16,11 +16,5 @@ public class DisplayGUI : MonoBehaviour {
 	void OnGUI () {
 		moneyText.GetComponent<Text>().text = "" + GameControl.control.money;
 		milkText.GetComponent<Text>().text = "" + GameControl.control.milk;
-
-		moneyText.GetComponent<Text> ().fontSize = (int)(26 * GameControl.control.screenMulti);
-		milkText.GetComponent<Text> ().fontSize = (int)(26 * GameControl.control.screenMulti);
-
-		moneyIcon.GetComponent<Image> ().transform.localScale = new Vector3(0.5f * GameControl.control.screenMulti, 0.5f * GameControl.control.screenMulti, 1.0f);
-		milkIcon.GetComponent<Image> ().transform.localScale = new Vector3(0.25f * GameControl.control.screenMulti, 0.25f * GameControl.control.screenMulti, 1.0f);
 	}
 }
