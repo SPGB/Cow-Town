ef6be26668e2863e6e3aa41ce70659519e05b6d5
Sam Gallagher <avonwodahs@gmail.com>
2015-08-16 22:40:18 -0400

    Updating background, canvas scaler, releasing patch 106

--- Assets/sky.cs
+++ Assets/sky.cs
@@ -3,13 +3,8 @@ using System.Collections;
 
 public class sky : MonoBehaviour {
 
-	public float speed = 0.1f;
+	public float speed = 0.0001f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (!GameControl.control.pause) transform.Rotate(0f, 0f, speed);
