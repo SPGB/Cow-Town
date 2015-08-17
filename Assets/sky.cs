ef2b1e6e4dcc5c9fae2ee26ad4988b45dd0737ac
Sam Gallagher <avonwodahs@gmail.com>
2015-08-17 09:16:59 -0400

    hotfix, slowing down background rotation

--- Assets/sky.cs
+++ Assets/sky.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class sky : MonoBehaviour {
 
 	public float speed = 0.0001f;
-
+	public float seconds = 1;
+	public bool is_spinning = false;
 	// Update is called once per frame
-	void Update () {
+	IEnumerator spin(float seconds) {
+		yield return new WaitForSeconds(seconds);
 		if (!GameControl.control.pause) transform.Rotate(0f, 0f, speed);
+		is_spinning = false;
+	}
+
+	void Update () {
+		if (!is_spinning) {
+			is_spinning = true;
+			StartCoroutine(spin(seconds));
+		}
 	}
 }
