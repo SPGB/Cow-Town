5bf114725b9765f127937838ba9e2616354d40b5
L4w3s <l4w3s@live.co.uk>
2015-08-18 18:58:48 +0100

    Added a real time day/night rotation cycle for the sky, it is currently commented out however as it has not been
    discussed with the rest of the team. The sky image file has been rotated
    by 180 degrees, but the rotation value in unity has been set to 180 to
    display it with the sun on the top, the reason for the rotation was for
    the real time testing as the rotation in real time uses the system's
    time, and an angle of 0 matched a time of 00:00, which is in the middle
    of the night and not day (thus needing the moon at the top).

--- Assets/sky.cs
+++ Assets/sky.cs
@@ -9,6 +9,8 @@ public class sky : MonoBehaviour {
 	// Update is called once per frame
 	IEnumerator spin(float seconds) {
 		yield return new WaitForSeconds(seconds);
+		// Below was used for testing a real time day/night cycle.
+		//if (!GameControl.control.pause) transform.rotation = Quaternion.Euler (0.0f, 0.0f, (float)System.DateTime.Now.TimeOfDay.TotalMinutes / 4);
 		if (!GameControl.control.pause) transform.Rotate(0f, 0f, speed);
 		is_spinning = false;
 	}
@@ -18,5 +20,13 @@ public class sky : MonoBehaviour {
 			is_spinning = true;
 			StartCoroutine(spin(seconds));
 		}
+
+		/**
+		 * Below was used for testing a real time day/night cycle.
+		 * 
+		float angle = (float)System.DateTime.Now.TimeOfDay.TotalMinutes;
+		Debug.Log ("SKY ANGLE: " + (angle / 4) + " / MIN ANGLE: " + (((0 * 60) + 0) / 4) + " / MAX ANGLE: " + (((23 * 60) + 59) / 4));
+		transform.rotation = Quaternion.Euler (0.0f, 0.0f, angle / 4);
+		**/
 	}
 }
